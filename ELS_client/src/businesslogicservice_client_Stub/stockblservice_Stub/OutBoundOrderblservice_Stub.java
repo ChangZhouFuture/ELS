82062678b8d2e690441b6900ee9c96db1a317f85
zhouchang <273444596@qq.com>
2015-10-24 17:27:14 +0800

    完成了出库单业务逻辑的桩和驱动

--- ELS_client/src/businesslogicservice_client_Stub/stockblservice_Stub/OutBoundOrderblservice_Stub.java
+++ ELS_client/src/businesslogicservice_client_Stub/stockblservice_Stub/OutBoundOrderblservice_Stub.java
@@ -1,45 +1,104 @@
 package businesslogicservice_client_Stub.stockblservice_Stub;
 
+import java.util.ArrayList;
+
 import businesslogicservice_client.stockblservice.OutBoundOrderblservice;
 import vo_client.OrderVO;
 import vo_client.stockVO.OutBoundOrderVO;
 
 public class OutBoundOrderblservice_Stub implements OutBoundOrderblservice{
-
+	OutBoundOrderVO oboVO;
+	OrderVO oVO;
+	String id4obo;
+	String id4e;
+	String id4to;
+	String desti;
+	String time;
+	
 	@Override
-	public OrderVO addExpress(long id) {
+	public OrderVO addExpress(String id) {
 		// TODO Auto-generated method stub
-		return null;
+		// 此处调用数据层的查找方法
+		
+		System.out.println("增加快件成功");
+		return oVO;
 	}
 
 	@Override
-	public OutBoundOrderVO add(long id4e, String date, String desti, long id4to) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void delete(long id) {
+	public void generateId() {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public OutBoundOrderVO modify(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public OutBoundOrderVO inquire(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void endOBO() {
+	public void generateTime() {
 		// TODO Auto-generated method stub
 		
 	}
 	
+	@Override
+	public OutBoundOrderVO add(String id4e, String id4to, String desti) {
+		// TODO Auto-generated method stub
+		this.id4e = id4e;
+		this.id4to = id4to;
+		this.desti = desti;
+//		此处传入VO除id外的所有参数，并由VO类里面generateID方法生成单号，记录oboVO（PO和VO的单号是一样的）；
+		oboVO = new OutBoundOrderVO(this.id4e, this.desti, this.id4to, this.id4obo, this.time);
+		
+		
+//		此处调用数据层的增加出库单方法，更新PO
+		
+		return oboVO;
+	}
+
+	@Override
+	public void deleteone(String id4bob) {
+		// TODO Auto-generated method stub
+		//处理删除 调用数据层方法 更新 PO
+		
+		//处理删除 更新VO
+		
+		System.out.println("删除一个出库单成功");
+	}
+	
+	@Override
+	public void deleteMany(ArrayList<String> id4OBO) {
+		// TODO Auto-generated method stub
+		//处理删除 调用数据层方法 更新PO
+		
+		//处理删除 更新VO
+		
+		System.out.println("删除多个出库单成功");
+	}
+
+	@Override
+	public OutBoundOrderVO modify(String id4obo, String id4e, String id4to,
+			String desti) {
+		// TODO Auto-generated method stub
+		//调用数据层的修改方法，修改PO
+		
+		return null;
+	}
+	
+	@Override
+	public OutBoundOrderVO inquireA(String id) {
+		// TODO Auto-generated method stub
+		
+		return oboVO;
+	}
+	
+	@Override
+	public OutBoundOrderVO inquireB(String time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void endOBO() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("结束对出库单处理");
+	}
+
+
 }
--- ELS_client/src/vo_client/stockVO/OutBoundOrderVO.java
+++ ELS_client/src/vo_client/stockVO/OutBoundOrderVO.java
@@ -1,5 +1,58 @@
 package vo_client.stockVO;
 
-public class OutBoundOrderVO {
+import java.io.Serializable;
 
+public class OutBoundOrderVO implements Serializable{
+	String id4e;
+	
+	String desti;
+	
+	String id4to;
+	
+	String id4obo;
+	
+	String time;
+	
+	public OutBoundOrderVO(String id4e, String desti, String id4to, String id4obo, 
+			String time){
+		super();
+		this.id4e = id4e;
+		this.desti = desti;
+		this.id4to = id4to;
+		this.id4obo = id4obo;
+		this.time = time;
+	}
+
+	public String getId4obo() {
+		return this.id4obo;
+	}
+	
+	public String getTime() {
+		return this.time;
+	}
+	
+	public String getId4e() {
+		return id4e;
+	}
+
+	public void setId4e(String id4e) {
+		this.id4e = id4e;
+	}
+
+	public String getDesti() {
+		return desti;
+	}
+
+	public void setDesti(String desti) {
+		this.desti = desti;
+	}
+
+	public String getId4to() {
+		return id4to;
+	}
+
+	public void setId4to(String id4to) {
+		this.id4to = id4to;
+	}
+	
 }
