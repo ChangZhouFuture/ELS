5dee43c466a16d8c2c4d002c3e2ac267da382a9f
zhouchang <273444596@qq.com>
2015-11-29 12:00:24 +0800

    司机lineitemPO以及对能否同步的测试

--- ELS_client/src/po/lineitemPO/inforManagementlineitemPO/DriverslineitemPO.java
+++ ELS_client/src/po/lineitemPO/inforManagementlineitemPO/DriverslineitemPO.java
@@ -1,7 +1,44 @@
 package po.lineitemPO.inforManagementlineitemPO;
 
 import java.io.Serializable;
+import state.Gender;
 
 public class DriverslineitemPO implements Serializable{
-
+	private String ID;
+	private String name;
+	private String phone;
+	private Gender gender;
+	private String driveLimitDate;
+	
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public String getDriveLimitDate() {
+		return driveLimitDate;
+	}
+	public void setDriveLimitDate(String driveLimitDate) {
+		this.driveLimitDate = driveLimitDate;
+	}
+	
 }
