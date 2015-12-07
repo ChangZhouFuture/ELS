52ac8453ae53fbeb77a6309c0f7685519f463b3c
zt0704@yahoo.com <zt0704@yahoo.com>
2015-12-07 20:34:23 +0800

    统计报表数据层

--- ELS_server/src/data/managerAndAccountantdata/StatisAnalydata.java
+++ ELS_server/src/data/managerAndAccountantdata/StatisAnalydata.java
@@ -2,35 +2,82 @@ package data.managerAndAccountantdata;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import bean.JavaBean1;
+import data.utility.Database;
+import data.utility.TimeCompare;
 import po.StatisAnalyPO.BusinessSituationPO;
 import po.StatisAnalyPO.CostAndIncomePO;
+import state.ResultMessage;
 import dataservice.managerAndAccountantdataservice.StatisAnalydataservice;
 
 
 public class StatisAnalydata extends UnicastRemoteObject implements StatisAnalydataservice{
 
+	Database db=new Database();
+    Connection con=db.getConnection();
+    JavaBean1 jb1;
+    PreparedStatement stmt;
+    BusinessSituationPO businessSituationPO;
+    CostAndIncomePO costAndIncomePO;
+    TimeCompare timeCompare;
+    
 	public StatisAnalydata() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public BusinessSituationPO BS(String StartDate, String EndDate) {
+	public JavaBean1 BusinessSituation(String StartDate, String EndDate) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public CostAndIncomePO CAI() {
+	public JavaBean1 CostAndIncome(String date) {
 		// TODO Auto-generated method stub
-		return null;
+		jb1=new JavaBean1();
+		jb1.setResultMessage(ResultMessage.NotExist);
+		timeCompare=new TimeCompare();
+		costAndIncomePO=new CostAndIncomePO();
+		String sql="select * from receivablesorde";
+		double income=0;
+		double cost=0;
+		double profit;
+		try {
+			stmt=con.prepareStatement(sql);
+			ResultSet rs=stmt.executeQuery();
+			while(rs.next()){
+				if(timeCompare.dateCompare(date, rs.getString("date"))==3){
+					income=income+rs.getDouble("amount");
+					jb1.setResultMessage(ResultMessage.Success);
+				}
+			}
+			sql="select * from paymentorder";
+			stmt=con.prepareStatement(sql);
+			rs=stmt.executeQuery();
+			while(rs.next()){
+				if(timeCompare.dateCompare(date, rs.getString("date"))==3){
+					cost=cost+rs.getDouble("amount");
+					jb1.setResultMessage(ResultMessage.Success);
+				}
+			}profit=income=cost;
+			costAndIncomePO.setDate(date);
+			costAndIncomePO.setIncome(income);
+			costAndIncomePO.setCost(cost);
+			costAndIncomePO.setProfit(profit);
+			jb1.setObject(costAndIncomePO);
+			return jb1;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return jb1;
+		}
 	}
 
-	@Override
-	public double profit(double income, double cost) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 }
--- ELS_server/src/dataservice/managerAndAccountantdataservice/StatisAnalydataservice.java
+++ ELS_server/src/dataservice/managerAndAccountantdataservice/StatisAnalydataservice.java
@@ -2,9 +2,7 @@ package dataservice.managerAndAccountantdataservice;
 
 import java.rmi.Remote;
 
-import po.StatisAnalyPO.BusinessSituationPO;
-import po.StatisAnalyPO.CostAndIncomePO;
-
+import bean.JavaBean1;
 /**
  * 该类处理和查看统计分析有关的数据层方法
  * @author zt
@@ -18,21 +16,14 @@ public interface StatisAnalydataservice extends Remote{
 	 * @param EndDate
 	 * @return
 	 */
-	public BusinessSituationPO BS(String StartDate,String EndDate);
+	public JavaBean1 BusinessSituation(String StartDate,String EndDate);
 	
 	/**
 	 * 前置条件：
 	 * 后置条件：生成截止当前日期的成本收益表（总收入、总支出、总利润=总收入-总支出）返回给逻辑层
 	 * @return
 	 */
-	public CostAndIncomePO CAI();
+	public JavaBean1 CostAndIncome(String date);
 	
-	/**
-	 * 前置条件：已知总收入和总支出
-	 * 后置条件：根据总收入和总支出计算总利润返回给逻辑层
-	 * @param income
-	 * @param cost
-	 * @return
-	 */
-	public double profit(double income,double cost);
+
 }
