d6f01ce399ab9b9c46bbb328ea4179cac66dd489
zjwnju <459278852@qq.com>
2016-01-01 23:59:29 +0800

    调整分区

--- ELS_client/src/presentation/stockui/stockui/StockCountui.java
+++ ELS_client/src/presentation/stockui/stockui/StockCountui.java
@@ -5,11 +5,14 @@ import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -17,7 +20,6 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import bean.JavaBean3;
-import businesslogic.inforManagementbl.DriversInfor;
 import businesslogic.stockbl.Stock;
 import businesslogicservice.stockblservice.Stockblservice;
 import presentation.reuse.Images;
@@ -28,6 +30,8 @@ public class StockCountui extends JPanel{
 	public JTable table;
 	public DefaultTableModel tableModel;
 	public JButton adjust;
+	public JComboBox areaType;
+	String areaTypeValue="机动区";
 	JScrollPane scrollPane;
 	JavaBean3 javaBean3;
 	Stockblservice stockblservice;
@@ -35,6 +39,8 @@ public class StockCountui extends JPanel{
 	public StockCountui(){
 		sheetLabel=new JLabel();
 		adjust=new JButton();
+		String[] areaTypeEntries={"机动区","汽运区","铁运区","航运区"};
+		areaType=new JComboBox(areaTypeEntries);
 		String[] columnNames = {"选择","ID","目的地","区号","排号","架号","位号","入库日期"}; //列名
 		String [][]tableVales={}; //数据
 		tableModel = new DefaultTableModel(tableVales,columnNames);
@@ -88,6 +94,19 @@ public class StockCountui extends JPanel{
 		scrollPane.setLocation(30,50);
 		scrollPane.setViewportView(table);
 		
+		areaType.setBounds(30,430,100,24);
+		areaType.setBackground(Color.WHITE);
+		areaType.setFont(font2);
+		areaType.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == ItemEvent.SELECTED){
+					areaTypeValue=(String)areaType.getSelectedItem();
+				}
+			}
+		});
+		
 		adjust.setBounds(150,430,50,24);
 		adjust.setBorder(BorderFactory.createLineBorder(Color.lightGray));
 		adjust.setBackground(Color.WHITE);
@@ -99,16 +118,16 @@ public class StockCountui extends JPanel{
 				   while(table.getSelectedRow()>=0){
 				       idList.add((String)table.getValueAt(table.getSelectedRow(),1));
 				   }
-//				   stockblservice.adjustPartition(idList,area);
+				   stockblservice.adjustPartition(idList,areaTypeValue);
 				  }});
 		
 		this.add(sheetLabel);
 		this.add(scrollPane);
 		this.add(adjust);
+		this.add(areaType);
 		setLocation(184,30);
 		this.setSize(616,496);
 		this.setBackground(Color.WHITE);
 		this.setBorder(BorderFactory.createLineBorder(Color.lightGray));
-		this.setOpaque(true);
 	}
 }
