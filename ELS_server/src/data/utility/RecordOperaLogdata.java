a7882fac5e4b094e76b20155901ffe3be3a8727e
zt0704@yahoo.com <zt0704@yahoo.com>
2016-01-02 18:56:21 +0800

    记录操作日志

--- ELS_server/src/data/utility/RecordOperaLogdata.java
+++ ELS_server/src/data/utility/RecordOperaLogdata.java
@@ -23,30 +23,19 @@ public class RecordOperaLogdata extends UnicastRemoteObject implements RecordOpe
 		// TODO Auto-generated constructor stub
 	}
 
+
 	@Override
-	public ResultMessage recordOperaLog(OperaType type, String date, String objectType, String id, String operatorId) {
-		// TODO Auto-generated method stub
+	public ResultMessage recordOperaLog(OperaType type, String date, String objectType, String id, Position position,
+			String operatorId) throws RemoteException {
 		String sql="insert into operalog(operaType,date,objectType,ID,position,operatorID) values(?,?,?,?,?,?)";
-		String position=null;
 		try {
 			stmt=con.prepareStatement(sql);
 			stmt.setString(1, type.toString());
 			stmt.setString(2, date);
 			stmt.setString(3, objectType);
 			stmt.setString(4, id);
+			stmt.setString(5, position.toString());
 			stmt.setString(6, operatorId);
-			String sub=operatorId.substring(0,2);
-			switch(sub){
-			case "00":position="Accountant1";break;
-			case "01":position="Accountant2";break;
-			case "02":position="Administrator";break;
-			case "03":position="BusiHallClerk";break;
-			case "04":position="Courier";break;
-			case "05":position="GeneralManager";break;
-			case "06":position="StockManager";break;
-			case "07":position="TranCenClerk";break;
-			}
-			stmt.setString(5, position);
 			stmt.executeUpdate();
 			return ResultMessage.Success;
 		} catch (SQLException e) {
