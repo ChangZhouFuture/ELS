357128cc57f19503a127cf5380b6fa974bcb10e9
zhouchang <273444596@qq.com>
2016-01-02 18:52:36 +0800

    记录操作日志dataservice

--- ELS_client/src/dataservice/utilitydataservice/RecordOperaLogdataservice.java
+++ ELS_client/src/dataservice/utilitydataservice/RecordOperaLogdataservice.java
@@ -2,11 +2,11 @@ package dataservice.utilitydataservice;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
 import state.OperaType;
+import state.Position;
 import state.ResultMessage;
 
 public interface RecordOperaLogdataservice extends Remote{
 	public ResultMessage recordOperaLog (OperaType type, String date, String objectType,
-			String id, String operatorId) throws RemoteException;
+			String id, Position position, String operatorId) throws RemoteException;
 }
